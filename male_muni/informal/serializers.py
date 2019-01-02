c901aa23b3b4d37e3f4f5be4c001b1ff5c1e5f20
Estanislau Marques <estanislaumarques@gmail.com>
2019-01-02 17:01:15 +0200

    Added vendedor serializer class to call all fields on json response

--- male_muni/informal/serializers.py
+++ male_muni/informal/serializers.py
@@ -1,18 +1,24 @@
 from informal.models import Item
 from informal.models import Produto
+from informal.models import Vendedor
 
 from rest_framework import serializers
 
 
-class ItemSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = Item
-		fields = '__all__'
-
-
-class ProdutoSerializer(serializers.HyperlinkedModelSerializer):
+class ProdutoSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Produto
 		fields = ('id', 'nome',)
 
+class VendedorSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Vendedor
+		fields = '__all__'
 
+class ItemSerializer(serializers.ModelSerializer):
+	produto = ProdutoSerializer()
+	vendedor = VendedorSerializer()
+
+	class Meta:
+		model = Item
+		fields = '__all__'
